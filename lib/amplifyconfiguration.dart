d0d25f766bd1a5ff3242f768ba9e9d22e59de0d8
SwastikPurathepparambil <72681039+SwastikPurathepparambil@users.noreply.github.com>
2022-06-28 14:32:26 -0700

    Update amplifyconfiguration.dart

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,1 +1,68 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "removed",
+                            "Region": "us-west-2"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "removed",
+                        "AppClientId": "removed",
+                        "Region": "us-west-2"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "OAuth": {
+                           "WebDomain": "removed",
+                           "AppClientId": "removed",
+                           "SignInRedirectURI": "myapp://",
+                           "SignOutRedirectURI": "myapp://",
+                           "Scopes": [
+                               "phone",
+                               "email",
+                               "openid",
+                               "profile",
+                               "aws.cognito.signin.user.admin"
+                           ]
+                        },
 
+
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [
+                            "GOOGLE"
+                        ],
+                        "usernameAttributes": [],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
